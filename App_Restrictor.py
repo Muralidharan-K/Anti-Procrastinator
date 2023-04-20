65afcb6a4dd6b5116b3fceccd4be02ef96cae1bb
Muralidharan-K <muralidharank25@gmail.com>
2023-04-20 19:03:09 +0530

    Updates done

--- App_Restrictor.py
+++ App_Restrictor.py
@@ -4,21 +4,21 @@ import pickle
 import psutil
 
 while True:
-    username=getpass.getuser()
-    filename=r'%s.pk' % (username)
+    username = getpass.getuser()
+    filename = r'%s.pk' % (username)
     try:
-        with open (filename, 'rb') as file:
-            emp_blacklist=pickle.load(file)
+        with open(filename, 'rb') as file:
+            emp_blacklist = pickle.load(file)
     except FileNotFoundError:
-        emp_blacklist=dict()
+        emp_blacklist = dict()
     for proc in psutil.process_iter(['name', 'exe']):
         try:
             app_n = proc.info['name'].split('.')[0]
-            app_name=str(app_n).lower()
+            app_name = str(app_n).lower()
             app_p = proc.info['exe']
-            app_path=str(app_p).lower()
+            app_path = str(app_p).lower()
             for key in list(emp_blacklist.keys()):
-               value=emp_blacklist[key]
+               value = emp_blacklist[key]
                if not isinstance(value, list):
                    if value in app_path:
                        proc.kill()
@@ -30,5 +30,5 @@ while True:
                        pass
         except:
             pass
-    key_list=list(emp_blacklist.keys())
+    key_list = list(emp_blacklist.keys())
     time.sleep(5)
